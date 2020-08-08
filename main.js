e4ab7ef5e8206ba4a7b83513ce8b0f33c42717e6
Abykirwa1937 <kkirwa@kabarak.ac.ke>
2020-08-08 10:36:37 +0300

    first commit

---
+++ main.js
@@ -0,0 +1,5 @@
+const signUpButton = document.getElementsByClassName("buttonz");
+const container = document.getElementsByClassName("container");
+
+signUpButton.addEventListener('click', () => container.classList.add('form group'))
+
